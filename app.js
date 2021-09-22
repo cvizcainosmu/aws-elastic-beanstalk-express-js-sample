cbef276f85c3a6553e31b4c92a0cdcae8992ea83
Carlos V <carlos@vizcaino.com>
2021-09-22 16:29:09 -0500

    Full pipeline test. Meow

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Life before death. Strength before weakness. Journey before destination.'));
+app.get('/', (req, res) => res.send('Here comes the CAT CAT KITTY KITTY CAT'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
